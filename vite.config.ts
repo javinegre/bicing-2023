e49cce21184fc152ccc042c2267df58847c83323
Javier Negre <javi.negre@gmail.com>
2022-12-02 18:21:06 +0100

    Load vite plugins depending on env

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import mkcert from 'vite-plugin-mkcert';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: { https: true },
-  plugins: [react(), mkcert(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  const envPlugins = isProd ? [] : [mkcert()];
+
+  return {
+    server: { https: true },
+    plugins: [tsconfigPaths(), react(), ...envPlugins],
+  };
 });

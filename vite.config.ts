20ff2b09efd273957185949a5ffd270cd43930a4
Javier Negre <javi.negre@gmail.com>
2022-12-03 23:12:32 +0100

    Install vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
+import svgLoader from 'vite-svg-loader';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -10,6 +11,6 @@ export default defineConfig(({ mode }) => {
 
   return {
     server: { https: true },
-    plugins: [tsconfigPaths(), react(), ...envPlugins],
+    plugins: [tsconfigPaths(), react(), svgLoader(), ...envPlugins],
   };
 });

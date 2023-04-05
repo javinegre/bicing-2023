dd24a3366f98a5cb0a74200e2ee69ad3730398dd
Javier Negre <javi.negre@gmail.com>
2023-04-05 21:03:32 +0200

    feat: icons update ...
     - improved bike and gears
     - added close and settings
     - fixes for CustomSvgIcon comp

--- src/components/Icons/CustomSvgIcon.tsx
+++ src/components/Icons/CustomSvgIcon.tsx
@@ -2,23 +2,37 @@ import React, { FC } from 'react';
 import BikeSvgIcon from '@assets/icons/ui/bike.svg?raw';
 import BoltSvgIcon from '@assets/icons/ui/bolt.svg?raw';
 import BriefcaseSvgIcon from '@assets/icons/ui/briefcase.svg?raw';
+import CloseSvgIcon from '@assets/icons/ui/close.svg?raw';
 import GearsSvgIcon from '@assets/icons/ui/gears.svg?raw';
 import HomeSvgIcon from '@assets/icons/ui/home.svg?raw';
 import ParkingSvgIcon from '@assets/icons/ui/parking.svg?raw';
+import SettingsSvgIcon from '@assets/icons/ui/settings.svg?raw';
 import StarSvgIcon from '@assets/icons/ui/star.svg?raw';
 import UserLocationSvgIcon from '@assets/icons/ui/user-location.svg?raw';
 import Box from '@mui/material/Box/Box';
 import { SxProps, Theme, useTheme } from '@mui/material/styles';
 
-type Icon = 'bike' | 'bolt' | 'briefcase' | 'gears' | 'home' | 'parking' | 'star' | 'user-location';
+type Icon =
+  | 'bike'
+  | 'bolt'
+  | 'briefcase'
+  | 'close'
+  | 'gears'
+  | 'home'
+  | 'parking'
+  | 'settings'
+  | 'star'
+  | 'user-location';
 
 const config: Record<Icon, string> = {
   bike: BikeSvgIcon,
   bolt: BoltSvgIcon,
   briefcase: BriefcaseSvgIcon,
+  close: CloseSvgIcon,
   gears: GearsSvgIcon,
   home: HomeSvgIcon,
   parking: ParkingSvgIcon,
+  settings: SettingsSvgIcon,
   star: StarSvgIcon,
   ['user-location']: UserLocationSvgIcon,
 };
@@ -38,14 +52,23 @@ const CustomSvgIcon: FC<{
   const sizeSx = size
     ? {
         width: `${+size}px`,
+        minWidth: `${+size}px`,
         height: `${+size}px`,
+        minHeight: `${+size}px`,
       }
-    : { width: defaultSize, height: defaultSize };
+    : { width: defaultSize, minWidth: defaultSize, height: defaultSize, minHeight: defaultSize };
   const pathFillSx = pathFill ? { path: { fill: pathFill } } : {};
 
-  const _sx = { fill: theme.palette.text.primary, ...pathFillSx, ...sizeSx, ...sxProps };
+  const _sx = {
+    fill: theme.palette.text.primary,
+    ...pathFillSx,
+    ...sizeSx,
+    ...sxProps,
+  };
 
-  return <Box display="flex" sx={_sx} dangerouslySetInnerHTML={{ __html: svg }} />;
+  return (
+    <Box display="flex" position="relative" sx={_sx} dangerouslySetInnerHTML={{ __html: svg }} />
+  );
 };
 
 export default CustomSvgIcon;
